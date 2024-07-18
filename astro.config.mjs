2447907e2eb7881db7d0c58aa80d6feaea35faf2
Lib <areeb.ali@delta.com>
2024-07-18 14:34:44 -0700

    second push

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,15 +13,36 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Fate/Grand Order',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
+						{ label: 'Main Story', link: '/fgo/main-story/' },
+						{ label: 'Events', link: '/fgo/events/' },
+						{ label: 'Servants', link: '/fgo/servants/' },
+					],
+				},
+				/*{
+					label: 'Source Material',
+					autogenerate: { directory: 'reference' },
+				},*/
+				{
+					label: 'Source Material',
+					items: [
+						{ label: 'Light Novels', link: '/source/light-novels/' },
+						{ label: 'Visual Novels', link: '/source/visual-novels/' },
+						{ label: 'Drama CDs', link: '/source/drama-cd/' },
+						{ label: 'Games', link: '/source/games/' },
+						{ label: 'Anime & Manga', link: '/source/anime-manga' },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Supplementals',
+					items: [
+						{ label: 'Materials', link: '/meta/materials/' },
+						{ label: 'Interviews', link: '/meta/interviews' },
+						{ label: 'Bamboo Diary', link: '/meta/bamboo-diary' },
+						{ label: 'Community Content', link: '/meta/community/'},
+					],
 				},
 			],
 		}),
